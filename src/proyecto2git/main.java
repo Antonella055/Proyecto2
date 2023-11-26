4cf398a06773913de6c45faa5805948e71cd9b47
Antonella055 <ma.ruiz@correo.unimet.edu.ve>
2023-11-26 12:26:02 -0400

    Final

---
+++ src/proyecto2git/main.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto2git;
+
+import interfaces.Menu;
+
+/**
+ *
+ * @author Antonella
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new Menu().setVisible(true);
+        // TODO code application logic here
+    }
+    
+}
